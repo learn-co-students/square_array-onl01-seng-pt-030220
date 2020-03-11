8089324ae0efaaf666794eb36d24b83f08354a0b
pleasant-archiver-6104 <marzettyakura@yahoo.com>
2020-03-11 00:05:47 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
-  # your code here
+  my_array = []
+  array.each do |n|
+    square = n ** 2
+    my_array << square
 end
+my_array
+end
+
+
+
+
+
+## make an empty array
+## iterate the old array
+## make a square variable and inside the variable
+## use exponents
+## shovel in square variable in new array
+## return new array
