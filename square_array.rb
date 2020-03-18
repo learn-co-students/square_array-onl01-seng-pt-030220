4a1ec5938a834a2b623d1859adf7d78733cd20f7
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-18 22:05:57 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+   new_array = []
+   
+   array.each do |array| # if i only used the #each i would get back the original array returned to me everytime
+   
+    new_array << (array ** 2)
+   end
+   return new_array
 end
